cc8d8b8271d02ff0f53605a587064d9dbd510311
s4721069 <s4721069@gmail.com>
2016-10-17 22:07:32 +0700

    aa

--- bot.php
+++ bot.php
@@ -9,7 +9,21 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) 
 {
 	// Loop through each event
-	foreach ($events['events'] as $event) {
+	foreach ($events['events'] as $event) 
+	{
+		$userId=$event['source']['userId'];
+		$url = 'https://api.line.me/v2/bot/profile/'.$userId;
+		$headers = 'Authorization: Bearer ' . $access_token;
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
 		{
@@ -58,7 +72,7 @@ if (!is_null($events['events']))
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => $replytext
+					'text' => $result
 				];
 
 				// Make a POST Request to Messaging API to reply to sender
