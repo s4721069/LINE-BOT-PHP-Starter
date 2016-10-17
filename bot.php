d5885d59e239a7cce7132eac40b0b46be3e456be
s4721069 <s4721069@gmail.com>
2016-10-17 22:12:56 +0700

    aa

--- bot.php
+++ bot.php
@@ -72,7 +72,7 @@ if (!is_null($events['events']))
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => $result
+					'text' => $result["Display name"];
 				];
 
 				// Make a POST Request to Messaging API to reply to sender
