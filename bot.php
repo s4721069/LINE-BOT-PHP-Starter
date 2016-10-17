fe7a7c1a6ff60e541c292dbca54f96ac2ae29298
s4721069 <s4721069@gmail.com>
2016-10-17 16:55:19 +0700

    a

--- bot.php
+++ bot.php
@@ -28,12 +28,12 @@ if (!is_null($events['events']))
 			}
 			else
 			{
-				$replytext=$scada_data->{'DateTimeZ7'};;
+				$replytext=$scada_data->{'DateTimeZ7'};
 			}
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $replytext
+				'text' => $content_scada
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
