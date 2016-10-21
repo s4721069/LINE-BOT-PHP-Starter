41271a1c90996f6c56c192e9be09f60c4ac0d1aa
s4721069 <s4721069@gmail.com>
2016-10-21 13:48:03 +0700

    aaa

--- bot.php
+++ bot.php
@@ -236,9 +236,12 @@ if (!is_null($events['events']))
 							$replytext2="อัตราการจ่าย\n";
 							$replytext2.="จ่ายเข้าเมือง ".number_format($scada_data['Z1SK_FE2_AINPUT_PV'],0)." ลบ.ม./ชม.\n";
 							$messages = [
-									'type' => 'text',
-									'text' =>  $replytext1
-								];					
+								'type' => 'text',
+								'text' =>  $replytext1
+								][
+								'type' => 'text',
+								'text' =>  $replytext2
+								];				
 							break;
 						default :
 							$replytext="ในขณะนี้ผมสามารถให้ข้อมูลของสาขาสงขลาได้ดังนี้\n";
