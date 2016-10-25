9b66c64488c6d7f68a4995d243bc9921dc644670
s4721069 <s4721069@gmail.com>
2016-10-25 22:51:52 +0700

    ok

--- bot.php
+++ bot.php
@@ -456,3 +456,17 @@ else
 	echo $result . "\r\n";
 }
 echo "OK";
+function resize($images,$new_images,$width)
+{
+	$size=GetimageSize($images);
+	$height=round($width*$size[1]/$size[0]);
+	$images_orig = ImageCreateFromJPEG($images);
+	$photoX = ImagesX($images_orig);
+	$photoY = ImagesY($images_orig);
+	$images_fin = ImageCreateTrueColor($width, $height);
+	ImageCopyResampled($images_fin, $images_orig, 0, 0, 0, 0, $width+1, $height+1, $photoX, $photoY);
+	ImageJPEG($images_fin,$new_images);
+	//ImageJPEG($images_fin,$new_images);
+	ImageDestroy($images_orig);
+	ImageDestroy($images_fin);
+}
