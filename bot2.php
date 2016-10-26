983e6f79010742614e7ce2687ca3c3b6fc33f7f4
s4721069 <s4721069@gmail.com>
2016-10-26 14:01:51 +0700

    aa

--- bot2.php
+++ bot2.php
@@ -382,7 +382,7 @@ if (!is_null($events['events']))
 				{
 					$content_dma = file_get_contents('http://dmamonitor.pwa.co.th/dashboard/services.php?method=dmaName&wwcode=5552011');
 					$dma_data = json_decode($content_dma, true);
-					$results = print_r($dma_data); // $results now contains output from print_r
+					$results = print_r($dma_data,true); // $results now contains output from print_r
 					$myfile = fopen("testfile.txt", "w");
 					fwrite($myfile, $results);
 					fclose($myfile);
