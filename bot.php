6472835f62462b5f2ce39302b2936f0cd42b9ed7
s4721069 <s4721069@gmail.com>
2016-11-13 20:04:02 +0700

    aa

--- bot.php
+++ bot.php
@@ -621,6 +621,9 @@ if (!is_null($events['events']))
 									'text' =>  $replytext1
 								]];
 							break;
+						case "DMA" :
+							$dmazone=strtoupper($textArr[3]);
+							f_dma("5552011","sk",$dmazone);
 						default :
 							$replytext="ในขณะนี้ผมสามารถให้ข้อมูลของสาขาสงขลาได้ดังนี้\n";
 							$replytext.="1. โรงสูบน้ำสำนักงาน ให้กรอก robot sk z1\n";
@@ -1054,4 +1057,163 @@ function resize($images,$new_images,$width)
 	//ImageJPEG($images_fin,$new_images);
 	ImageDestroy($images_orig);
 	ImageDestroy($images_fin);
+}
+
+function f_dma($wwcode,$shortcode,$dmazone)
+{
+	global $messages;
+
+	$upperdamzone=strtoupper($dmazone);
+	if(strlen($dmazone)>0)
+	{
+		$Latitude="";
+		$Longitude="";
+		$device_name="";
+		$sensor_Flow_LatestValue="";
+		$sensor_Flow_LastUpdated_date="";
+		$sensor_P2_LatestValue="";
+		$sensor_P2_LastUpdated_date="";
+		$sensor_Volume_LatestValue="";
+		$sensor_Volume_LastUpdated_date="";
+		$sensor_Battery_LatestValue="";
+		$sensor_Battery_LastUpdated_date="";
+
+		//http://dmamonitor.pwa.co.th/dashboard/services.php?method=device_detail&device_id=5542023-SL-MM-01
+		$content_dma = file_get_contents('http://dmamonitor.pwa.co.th/dashboard/services.php?method=device_detail&device_id='.$wwcode.'-SL-'.$upperdamzone);
+		$dma_data = json_decode($content_dma, true);
+
+		if(count($dma_data)>0)
+		{
+			foreach($dma_data as $k => $v) 
+			{
+				if(!is_array($v))
+				{
+						//$replytext.=$k." *** ".$v."\n";
+						if($k=="Latitude")
+							$Latitude=$v;
+						if($k=="Longitude")
+							$Longitude=$v;
+						if($k=="device_name")
+							$device_name=$v;
+				}
+				else
+				{
+					//$replytext.=$k."\n";
+					foreach($v as $k1 => $v1) 
+					{
+						if(!is_array($v1))
+						{
+							//$replytext.=$k1." *** ".$v1."\n";
+						}
+						else
+						{
+							//$replytext.=$k1."\n";
+							foreach($v1 as $k2 => $v2) 
+							{
+								if(!is_array($v2))
+								{
+									//$replytext.=$k2." *** ".$v2."\n";
+									if(($k=="sensor") && ($k1=="Flow") && ($k2=="LatestValue"))
+										$sensor_Flow_LatestValue=$v2;
+									if(($k=="sensor") && ($k1=="P 2 (Out)") && ($k2=="LatestValue"))
+										$sensor_P2_LatestValue=$v2;
+									if(($k=="sensor") && ($k1=="Volume") && ($k2=="LatestValue"))
+										$sensor_Volume_LatestValue=$v2;
+									if(($k=="sensor") && ($k1=="Battery Status") && ($k2=="LatestValue"))
+										$sensor_Battery_LatestValue=$v2;
+
+								}
+								else
+								{
+									//$replytext.=$k2."\n";
+									foreach($v2 as $k3 => $v3) 
+									{
+										if(!is_array($v3))
+										{
+											//$replytext.=$k3." *** ".$v3."\n";
+											if(($k=="sensor") && ($k1=="Flow") && ($k2=="LastUpdated") && ($k3=="date"))
+												$sensor_Flow_LastUpdated_date=$v3;
+											if(($k=="sensor") && ($k1=="P 2 (Out)") && ($k2=="LastUpdated") && ($k3=="date"))
+												$sensor_P2_LastUpdated_date=$v3;
+											if(($k=="sensor") && ($k1=="Volume") && ($k2=="LastUpdated") && ($k3=="date"))
+												$sensor_Volume_LastUpdated_date=$v3;
+											if(($k=="sensor") && ($k1=="Battery Status") && ($k2=="LastUpdated") && ($k3=="date"))
+												$sensor_Battery_LastUpdated_date=$v3;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			
+
+			$replytext1=$device_name;
+			if(strlen($sensor_Battery_LatestValue)>0)
+			{
+				$replytext1.="\n\nBattery Status ".$sensor_Battery_LatestValue." V.\n";
+				$replytext1.="ข้อมูล ณ ".$sensor_Battery_LastUpdated_date;
+			}
+			if(strlen($sensor_Flow_LatestValue)>0)
+			{
+				$replytext1.="\n\nอัตราการไหล ".$sensor_Flow_LatestValue." ลบ.ม./ชม.\n";
+				$replytext1.="ข้อมูล ณ ".$sensor_Flow_LastUpdated_date;
+			}
+			if(strlen($sensor_P2_LatestValue)>0)
+			{
+				$replytext1.="\n\nแรงดัน ".$sensor_P2_LatestValue." บาร์\n";
+				$replytext1.="ข้อมูล ณ ".$sensor_P2_LastUpdated_date;
+			}
+			if(strlen($sensor_Volume_LatestValue)>0)
+			{
+				$replytext1.="\n\nเลขมาตรขึ้น ".$sensor_Volume_LatestValue." ลบ.ม.\n";
+				$replytext1.="ข้อมูล ณ ".$sensor_Volume_LastUpdated_date;
+			}
+			
+			$messages = [[
+				'type' => 'text',
+				'text' =>  $replytext1
+			],[
+				'type' => 'location',
+				'title' =>  'ที่ตั้ง '.$device_name,
+				'address' =>  'latitude:'.$Latitude.' longitude:'.$Longitude,
+				'latitude' =>  $Latitude,
+				'longitude' =>  $Longitude
+			]];
+		}
+		else
+		{
+			$replytext="ไม่พบข้อมูล DMA ที่กรอก";
+			$messages = [[
+				'type' => 'text',
+				'text' =>  $replytext
+			]];
+		}
+
+	}
+
+	else
+	{
+		$content_dma = file_get_contents('http://dmamonitor.pwa.co.th/dashboard/services.php?method=dmaName&wwcode='.$wwcode);
+		$dma_data = json_decode($content_dma, true);
+		$replytext="มีข้อมูล DMA ดังนี้";
+		$arrDmaCode=array();
+		foreach($dma_data as $k => $v) 
+		{
+				//$replytext.=$k." *** ".$v."\n";
+				if(strlen($replytext)>0)
+					$replytext.="\n";
+			$arrDmaCode=explode($wwcode.'-SL-', $k);
+			$replytext.="-".$v." ให้กรอก robot ".$shortcode." dma ".strtolower($arrDmaCode[1]);
+		}
+
+		$messages = [[
+			'type' => 'text',
+			'text' =>  $replytext
+		]];
+
+	}
+
+
 }
