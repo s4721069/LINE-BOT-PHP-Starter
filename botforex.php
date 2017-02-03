cb1986577c4533204cbc48f69f1aec8449a3962c
s4721069 <s4721069@gmail.com>
2017-02-03 13:58:45 +0700

    aa

--- botforex.php
+++ botforex.php
@@ -13,6 +13,48 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) 
 {
+	// Loop through each event
+	foreach ($events['events'] as $event) 
+	{
+		
+
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
+		{
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			$textArr=explode(" ",$text);
+			if(strtoupper($textArr[0])=="ROBOT")
+			{
+				$replytext="สวัสดีครับ";
+				$messages = [[
+					'type' => 'text',
+					'text' =>  $replytext
+					]];
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => $messages,
+				];
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
+
+				echo $result . "\r\n";
+			}
+		}
+	}
 	
 }
 else
